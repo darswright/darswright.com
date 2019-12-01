7bb174625c9ba1d09e43e43ff1f2fda23bbc58e2
Simon Wright <darswright@DarsWright.local>
2019-12-01 11:10:42 +1000

    Completed Resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -33,6 +33,17 @@ export default function Resume(props) {
     )
   })
 
+  const skills = data.skills.map(item => {
+    const elClasses = `bar-expand ${item.class}`
+
+    return (
+      <li>
+        <span className={elClasses} />
+        <em>{item.skillname}</em>
+      </li>
+    )
+  })
+
   return (
     <section id="resume">
       <div className="row education">
@@ -67,39 +78,12 @@ export default function Resume(props) {
         </div>
 
         <div className="nine columns main-col">
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-            dolores eos qui ratione voluptatem sequi nesciunt.
+          <p>{data.skillsDescription}
           </p>
 
           <div className="bars">
             <ul className="skills">
-              <li>
-                <span className="bar-expand photoshop" />
-                <em>Photoshop</em>
-              </li>
-              <li>
-                <span className="bar-expand illustrator" />
-                <em>Illustrator</em>
-              </li>
-              <li>
-                <span className="bar-expand wordpress" />
-                <em>Wordpress</em>
-              </li>
-              <li>
-                <span className="bar-expand css" />
-                <em>CSS</em>
-              </li>
-              <li>
-                <span className="bar-expand html5" />
-                <em>HTML5</em>
-              </li>
-              <li>
-                <span className="bar-expand jquery" />
-                <em>jQuery</em>
-              </li>
+              {skills}
             </ul>
           </div>
         </div>
