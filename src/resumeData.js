e442079b367ca76c2e22e7656070c55653ab1f18
Simon Wright <darswright@DarsWright.local>
2019-12-01 11:13:28 +1000

    Added phone number and removed skype and linkedin ID

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,8 +2,7 @@ let resumeData = {
   "imagebaseurl": "https://rbhatia46.github.io/",
   "name": "Simon Wright",
   "role": "Frontend Developer",
-  "linkedinId": "Your LinkedIn Id",
-  "skypeid": "Your skypeid",
+  "number": "0481 270 550",
   "roleDescription": "I like to dabble in various parts of frontend web development and like to learn about new technologies.",
   "socialLinks": [
       {
@@ -48,7 +47,7 @@ let resumeData = {
       "jobOutline": [
         "Contract role assisting in-house developer in completing WooCommerce website",
         "Implemented Webpack and Git on website child theme and made recommendations on automating deployment and utilising a staging server",
-        "Recommended a better more robust and scalable hosting solution",
+        "Recommended a more robust and scalable hosting solution",
         "Troubleshooted page speed issues and made recommendations to improve overall website performance",
         "Rebuilt and integrated various components of the website that were previously hardcoded"
       ]
@@ -61,24 +60,72 @@ let resumeData = {
       "endMonth":"Jun",
       "endYear":"2019",
       "jobOutline": [
-        "Re-entered the web industry after a few years living overseas",
-        "Maintained legacy client websites built using an old bespoke CRM/CMS system",
+        "Re-entered the web industry after a few years living overseas and was initially tasked with maintaining legacy client websites built using an old bespoke CRM/CMS system",
         "Maintained WordPress websites built by off-shore team",
         "Managed the rebuilding of custom WordPress and WooCommerce projects",
         "Scoped project requirements, coordinated design phase, developed and tested projects and provided client training"
       ]
+    },
+    {
+      "CompanyName":"Sin Design + Branding",
+      "position":"WordPress Developer",
+      "startMonth":"Mar",
+      "startYear":"2012",
+      "endMonth":"Jul",
+      "endYear":"2014",
+      "jobOutline": [
+        "Was tasked with learning and implementing a new solution for building client websites, I chose WordPress",
+        "Learned Responsive Web Design fundamentals and began building responsive websites for our clients",
+        "Incorporated SASS and Grunt into the dev toolkit to improve CSS architecture and maintainability and automate tedious dev tasks"
+      ]
+    },
+    {
+      "CompanyName":"Graphics Online",
+      "position":"Web Developer",
+      "startMonth":"Oct",
+      "startYear":"2011",
+      "endMonth":"Mar",
+      "endYear":"2012",
+      "jobOutline": [
+        "Identical role to New Business Media"
+      ]
+    },
+    {
+      "CompanyName":"New Business Media",
+      "position":"Junior Web Developer",
+      "startMonth":"Oct",
+      "startYear":"2008",
+      "endMonth":"Jun",
+      "endYear":"2011",
+      "jobOutline": [
+        "Learned HTML, CSS & jQuery to a proficient level building website and email templates from Photoshop design mocks",
+        "Learned the fundamentals of database driven website development by integrating website templates into a PHP based CMS platform",
+        "WHM/cPanel account management, MySQL database creation and DNS setup",
+        "Assisted clients by providing phone technical support and troubleshooting"
+      ]
     }
   ],
-  "skillsDescription":"Your skills here",
+  "skillsDescription":"I focus heavily on front-end development. The front-end world has evolved so rapidly over the last few years it's almost terrifying! But at the same time I find it so interesting and am excited by how far it's come and where it's heading.",
   "skills":[
     {
-      "skillname": "HTML5"
+      "skillname": "HTML5",
+      "class": "html5"
     },
     {
-      "skillname": "CSS"
+      "skillname": "CSS/SASS (Bootstrap, Materialize)",
+      "class": "css"
     },
     {
-      "skillname": "Reactjs"
+      "skillname": "JavaScript (React, jQuery)",
+      "class": "jquery"
+    },
+    {
+      "skillname": "PHP (WordPress, WooCommerce)",
+      "class": "wordpress"
+    },
+    {
+      "skillname": "Photoshop",
+      "class": "photoshop"
     }
   ],
   "portfolio":[
@@ -101,16 +148,6 @@ let resumeData = {
       "name":"project4",
       "description":"mobileapp",
       "imgurl":"images/portfolio/phone.jpg"
-    }
-  ],
-  "testimonials":[
-    {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
-    },
-    {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
     }
   ]
 }
