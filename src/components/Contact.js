f7acd21b4b2aa419fa38e6bccefed319bd2cff31
Simon Wright <darswright@192-168-1-171.tpgi.com.au>
2020-01-12 21:55:43 +1000

    few bug fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact(props) {
-  const data = props.resumeData
+  const data = props.resumeData;
 
   const envelopeIcon = {
-    paddingRight: '10px',
-    fontSize: '72px',
-    lineHeight: '72px',
-    textAlign: 'left',
-    color: '#ebeeee'
-  }
+    paddingRight: "10px",
+    fontSize: "72px",
+    lineHeight: "72px",
+    textAlign: "left",
+    color: "#ebeeee"
+  };
 
   return (
     <section id="contact">
-      <div className="row section-head">
-        <div className="two columns header-col">
-          <h1>
-            <span>Get In Touch.</span>
-          </h1>
-          <FontAwesomeIcon style={envelopeIcon} icon={faEnvelope} />
-        </div>
-
-        <div className="ten columns">
-          <p className="lead">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. 
-          </p>
-        </div>
-      </div>
-
       <div className="row">
         <div className="eight columns">
           <form id="contactForm" name="contactForm" method="post">
@@ -42,25 +25,42 @@ export default function Contact(props) {
                 <label htmlFor="contactName">
                   Name <span className="required">*</span>
                 </label>
-                <input type="text" size="35" id="contactName" name="contactName" />
+                <input
+                  type="text"
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                />
               </div>
 
               <div>
                 <label htmlFor="contactEmail">
                   Email <span className="required">*</span>
                 </label>
-                <input type="email" size="35" id="contactEmail" name="contactEmail" />
+                <input
+                  type="email"
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                />
               </div>
 
               <div>
                 <label htmlFor="contactMessage">
                   Message <span className="required">*</span>
                 </label>
-                <textarea cols="50" rows="4" id="contactMessage" name="contactMessage" />
+                <textarea
+                  cols="50"
+                  rows="4"
+                  id="contactMessage"
+                  name="contactMessage"
+                />
               </div>
 
               <div>
-                <button className="submit" type="submit">Submit</button>
+                <button className="submit" type="submit">
+                  Submit
+                </button>
                 <span id="image-loader">
                   <img alt="Loading" src="./assets/images/loader.gif" />
                 </span>
@@ -71,7 +71,9 @@ export default function Contact(props) {
           <div id="message-warning"> Error boy</div>
 
           <div id="message-success">
-            <i className="fa fa-check" />Your message was sent, thank you!<br />
+            <i className="fa fa-check" />
+            Your message was sent, thank you!
+            <br />
           </div>
         </div>
 
@@ -79,8 +81,10 @@ export default function Contact(props) {
           <div className="widget widget_contact">
             <h4>Address and Phone</h4>
             <p className="address">
-              {data.name}<br />
-              {data.address}<br />
+              {data.name}
+              <br />
+              {data.address}
+              <br />
               <span>{data.email}</span>
             </p>
           </div>
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,50 +1,64 @@
 let resumeData = {
-  "imagebaseurl": "https://rbhatia46.github.io/",
-  "name": "Simon Wright",
-  "role": "Frontend Developer",
-  "number": "0481 270 550",
-  "roleDescription": "I like to dabble in frontend web development and learn about new web technologies.",
-  "socialLinks": [
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/in/darswright/"
-      },
-      {
-        "name":"codepen",
-        "url":"https://codepen.io/darswright"
-      },
-      {
-        "name":"github",
-        "url":"https://github.com/darswright"
-      }
-    ],
-  "aboutme":"I am a web developer with over 7 years experience building business websites for digital agencies on the Gold Coast.",
-  "address":"Robina, Gold Coast",
-  "email":"simmo.wright@gmail.com",
-  "resumeDownload": "Resume2019_SimonWright.pdf",
-  "education":[
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Simon Wright",
+  role: "Frontend Developer",
+  number: "0481 270 550",
+  roleDescription:
+    "I like to dabble in frontend web development and learn about new web technologies.",
+  socialLinks: [
     {
-      "UniversityName":"Griffith University",
-      "specialisation":"Bachelor of Multimedia",
-      "MonthOfPassing":"Nov",
-      "YearOfPassing":"2008"
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/darswright/"
     },
     {
-      "UniversityName":"Gold Coast Institute of TAFE",
-      "specialisation":"Diploma of Multimedia",
-      "MonthOfPassing":"Jun",
-      "YearOfPassing":"2005"
+      name: "codepen",
+      url: "https://codepen.io/darswright"
+    },
+    {
+      name: "github",
+      url: "https://github.com/darswright"
     }
   ],
-  "work":[
+  aboutme:
+    "I am a web developer with over 7 years experience building business websites for digital agencies on the Gold Coast.",
+  address: "Robina, Gold Coast",
+  email: "simmo.wright@gmail.com",
+  resumeDownload: "Resume2019_SimonWright.pdf",
+  education: [
     {
-      "CompanyName":"Grace Loves Lace",
-      "position":"WordPress Developer",
-      "startMonth":"Jul",
-      "startYear":"2019",
-      "endMonth":"Nov",
-      "endYear":"2019",
-      "jobOutline": [
+      UniversityName: "Griffith University",
+      specialisation: "Bachelor of Multimedia",
+      MonthOfPassing: "Nov",
+      YearOfPassing: "2008"
+    },
+    {
+      UniversityName: "Gold Coast Institute of TAFE",
+      specialisation: "Diploma of Multimedia",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2005"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "NewBook",
+      position: "Web Developer",
+      startMonth: "Nov",
+      startYear: "2019",
+      endMonth: "Present",
+      endYear: "",
+      jobOutline: [
+        "Currently working in the NewBook Creative team tasked with the setup and development of new client websites and booking pages",
+        "Also provide website support and maintenance"
+      ]
+    },
+    {
+      CompanyName: "Grace Loves Lace",
+      position: "WordPress Developer",
+      startMonth: "Jul",
+      startYear: "2019",
+      endMonth: "Nov",
+      endYear: "2019",
+      jobOutline: [
         "Contract role assisting in-house developer in completing WooCommerce website",
         "Implemented Webpack and Git on website child theme and made recommendations on automating deployment and utilising a staging server",
         "Recommended a more robust and scalable hosting solution",
@@ -53,13 +67,13 @@ let resumeData = {
       ]
     },
     {
-      "CompanyName":"Localsearch",
-      "position":"WordPress Developer",
-      "startMonth":"Apr",
-      "startYear":"2018",
-      "endMonth":"Jun",
-      "endYear":"2019",
-      "jobOutline": [
+      CompanyName: "Localsearch",
+      position: "WordPress Developer",
+      startMonth: "Apr",
+      startYear: "2018",
+      endMonth: "Jun",
+      endYear: "2019",
+      jobOutline: [
         "Re-entered the web industry after a few years living overseas and was initially tasked with maintaining legacy client websites built using an old bespoke CRM/CMS system",
         "Maintained WordPress websites built by off-shore team",
         "Managed the rebuilding of custom WordPress and WooCommerce projects",
@@ -67,37 +81,35 @@ let resumeData = {
       ]
     },
     {
-      "CompanyName":"Sin Design + Branding",
-      "position":"WordPress Developer",
-      "startMonth":"Mar",
-      "startYear":"2012",
-      "endMonth":"Jul",
-      "endYear":"2014",
-      "jobOutline": [
+      CompanyName: "Sin Design + Branding",
+      position: "WordPress Developer",
+      startMonth: "Mar",
+      startYear: "2012",
+      endMonth: "Jul",
+      endYear: "2014",
+      jobOutline: [
         "Was tasked with learning and implementing a new solution for building client websites, I chose WordPress",
         "Learned Responsive Web Design fundamentals and began building responsive websites for our clients",
         "Incorporated SASS and Grunt into the dev toolkit to improve CSS architecture and maintainability and automate tedious dev tasks"
       ]
     },
     {
-      "CompanyName":"Graphics Online",
-      "position":"Web Developer",
-      "startMonth":"Oct",
-      "startYear":"2011",
-      "endMonth":"Mar",
-      "endYear":"2012",
-      "jobOutline": [
-        "Identical role to New Business Media"
-      ]
+      CompanyName: "Graphics Online",
+      position: "Web Developer",
+      startMonth: "Oct",
+      startYear: "2011",
+      endMonth: "Mar",
+      endYear: "2012",
+      jobOutline: ["Identical role to New Business Media"]
     },
     {
-      "CompanyName":"New Business Media",
-      "position":"Junior Web Developer",
-      "startMonth":"Oct",
-      "startYear":"2008",
-      "endMonth":"Jun",
-      "endYear":"2011",
-      "jobOutline": [
+      CompanyName: "New Business Media",
+      position: "Junior Web Developer",
+      startMonth: "Oct",
+      startYear: "2008",
+      endMonth: "Jun",
+      endYear: "2011",
+      jobOutline: [
         "Learned HTML, CSS & jQuery to a proficient level building website and email templates from Photoshop design mocks",
         "Learned the fundamentals of database driven website development by integrating website templates into a PHP based CMS platform",
         "WHM/cPanel account management, MySQL database creation and DNS setup",
@@ -105,51 +117,52 @@ let resumeData = {
       ]
     }
   ],
-  "skillsDescription":"I focus heavily on front-end development. The front-end world has evolved so rapidly over the last few years it's almost terrifying! But at the same time I find it so interesting and am excited by how far it's come and where it's heading.",
-  "skills":[
+  skillsDescription:
+    "I focus heavily on front-end development. The front-end world has evolved so rapidly over the last few years it's almost terrifying! But at the same time I find it so interesting and am excited by how far it's come and where it's heading.",
+  skills: [
     {
-      "skillname": "HTML",
-      "class": "html5"
+      skillname: "HTML5",
+      class: "html5"
     },
     {
-      "skillname": "CSS/SASS (Bootstrap, Materialize)",
-      "class": "css"
+      skillname: "CSS/SASS (Bootstrap, Materialize)",
+      class: "css"
     },
     {
-      "skillname": "JavaScript (React, jQuery)",
-      "class": "jquery"
+      skillname: "JavaScript (React, jQuery)",
+      class: "jquery"
     },
     {
-      "skillname": "PHP (WordPress, WooCommerce)",
-      "class": "wordpress"
+      skillname: "WordPress",
+      class: "wordpress"
     },
     {
-      "skillname": "Photoshop",
-      "class": "photoshop"
+      skillname: "Photoshop",
+      class: "photoshop"
     }
   ],
-  "portfolio":[
+  portfolio: [
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
     },
     {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
     },
     {
-      "name":"project3",
-      "description":"mobileapp",  
-      "imgurl":"images/portfolio/project2.png"
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
     },
     {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
     }
   ]
-}
+};
 
-export default resumeData
+export default resumeData;
